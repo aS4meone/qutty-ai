e4466768bcebb73da3df18c4c839a624038d76b2
Alnur <zholdas.alnur@gmail.com>
2024-07-25 11:44:39 +0500

    feat: link to detail results added to results page

--- frontend/src/app/results/page.tsx
+++ frontend/src/app/results/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, {useEffect, useState} from "react";
-import Image from "next/image";
-import {GESTURE_IMAGES} from "@/lib/gestures";
+import {FaExternalLinkAlt} from 'react-icons/fa';
 import Link from "next/link";
 
 interface DataItem {
@@ -50,7 +49,7 @@ export default function Results() {
                     </Link>
                 </nav>
             </header>
-            <div className="max-w-4xl mx-auto p-4 bg-white shadow-md rounded-lg">
+            <div className="max-w-6xl mx-auto p-4 bg-white shadow-md rounded-lg">
                 <div className="mb-4">
                     <h2 className="text-2xl font-bold">Данные</h2>
                     <p className="text-gray-600">Таблица с информацией о пациентах.<br/>Максимальные баллы по тестам
@@ -79,6 +78,7 @@ export default function Results() {
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Тест
                                 4 (2)
                             </th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
                         </tr>
                         </thead>
                         <tbody className="bg-white divide-y divide-gray-200">
@@ -90,13 +90,19 @@ export default function Results() {
                                 <td className="px-6 py-4 whitespace-nowrap">{item.age}</td>
                                 {item.moca != null ?
                                     <td className="px-6 py-4 whitespace-nowrap">{item.moca}</td> :
-                                <td className="px-6 py-4 whitespace-nowrap">{item.diagnosis}</td>
-
+                                    <td className="px-6 py-4 whitespace-nowrap">{item.diagnosis}</td>
                                 }
                                 <td className="px-6 py-4 whitespace-nowrap text-center">{item.first_test}</td>
                                 <td className="px-6 py-4 whitespace-nowrap text-center">{item.second_test}</td>
                                 <td className="px-6 py-4 whitespace-nowrap text-center">{item.third_test}</td>
                                 <td className="px-6 py-4 whitespace-nowrap text-center">{item.fourth_test}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">
+                                    <Link href={`/results/${item.name}`} prefetch={false}>
+                                    <span className="text-blue-600 hover:text-blue-800 cursor-pointer">
+                                        <FaExternalLinkAlt size={20}/>
+                                    </span>
+                                    </Link>
+                                </td>
                             </tr>
                         ))}
                         </tbody>
